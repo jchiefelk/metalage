e6f5b45e10ee53d83159141a5d85b1aa7cf63e6c
Jackson Chief Elk <chiefelk@Jacksons-MacBook-Pro.local>
2019-10-01 06:41:39 -0700

    Add graphql endpoint

--- apps/metal/urls.py
+++ apps/metal/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from graphene_django.views import GraphQLView
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    url(r'history', views.get_data)
+    url(r'history', views.get_data),
+    path('graphql', GraphQLView.as_view(graphiql=True)),
 ]
