d4340e39ea60549e9857fafe7d50e0c6e090c143
Jackson Chief Elk <chiefelk@Jacksons-MacBook-Pro.local>
2019-11-01 16:53:12 -0700

    Add new filter fields

--- apps/metal/schema.py
+++ apps/metal/schema.py
@@ -3,12 +3,13 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from .models import Metal
 from .models import ElementTraceAssay
+from .models import IsotopeTraceAssay
 
 
 class MetalObjects(DjangoObjectType):
     class Meta:
         model = Metal
-        filter_fields = ['country']
+        filter_fields = ['country', 'site', 'museum_number', 'source']
         interfaces = (graphene.relay.Node, )
 
 
@@ -19,11 +20,18 @@ class ElementTraceAssayObjects(DjangoObjectType):
         interfaces = (graphene.relay.Node, )
 
 
+class IsotopeTraceAssayObjects(DjangoObjectType):
+    class Meta:
+        model = IsotopeTraceAssay
+        filter_fields = ['analysis_method', 'id']
+        interfaces = (graphene.relay.Node, )
+
+
 class Query(graphene.ObjectType):
     metal = graphene.relay.Node.Field(MetalObjects)
     all_metal = DjangoFilterConnectionField(MetalObjects)
     elements = graphene.relay.Node.Field(ElementTraceAssayObjects)
-    all_elements = DjangoFilterConnectionField(ElementTraceAssayObjects) 
-
+    all_elements = DjangoFilterConnectionField(ElementTraceAssayObjects)
+    all_isotopes = DjangoFilterConnectionField(IsotopeTraceAssayObjects)
 
 schema = graphene.Schema(query=Query)
