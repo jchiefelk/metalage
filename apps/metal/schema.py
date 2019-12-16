e9f9e89318a50d07fcab1a7cf6c1ad4e0c70be3e
Jackson Chief Elk <chiefelk@Jacksons-MacBook-Pro.local>
2019-12-16 08:19:33 -0800

    Add ore to model to graphql queries

--- apps/metal/schema.py
+++ apps/metal/schema.py
@@ -2,8 +2,16 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from .models import Metal
+from .models import Ore
 from .models import ElementTraceAssay
 from .models import IsotopeTraceAssay
+
+
+class OreObjects(DjangoObjectType):
+    class Meta:
+        model = Ore
+        filter_fields = ['country', 'site', 'museum_number', 'source', 'description']
+        interfaces = (graphene.relay.Node, )
 
 
 class MetalObjects(DjangoObjectType):
@@ -30,6 +38,8 @@ class IsotopeTraceAssayObjects(DjangoObjectType):
 class Query(graphene.ObjectType):
     metal = graphene.relay.Node.Field(MetalObjects)
     all_metal = DjangoFilterConnectionField(MetalObjects)
+    ore = graphene.relay.Node.Field(OreObjects)
+    all_ore = DjangoFilterConnectionField(OreObjects)
     elements = graphene.relay.Node.Field(ElementTraceAssayObjects)
     all_elements = DjangoFilterConnectionField(ElementTraceAssayObjects)
     all_isotopes = DjangoFilterConnectionField(IsotopeTraceAssayObjects)
